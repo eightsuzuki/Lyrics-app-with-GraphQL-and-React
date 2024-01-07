33b3d4b63281e69746b240505712f00c636caf12
eightsuzuki <suzukieight8@gmail.com>
2024-01-08 02:30:48 +0900

    Handle Pending Queries

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // Replace with your Mongo Atlas URI
 const MONGO_URI = 'mongodb+srv://suzukieight8:8qw6MSxccim9vqqC@cluster0.wsn0kwr.mongodb.net/?retryWrites=true&w=majority';
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+  throw new Error("You must provide a Mongo Atlas URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
+  .once("open", () => console.log("Connected to Mongo Atlas instance."))
+  .on("error", (error) =>
+    console.log("Error connecting to Mongo Atlas:", error)
   );
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
     graphiql: true
   })
 );
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
